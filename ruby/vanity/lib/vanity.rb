eba9971c229dff2f71522eb9abd721223ece3064
Rich Kuzsma <62522248+richkuz@users.noreply.github.com>
2020-09-30 06:39:27 -0400

    Having fun

--- ruby/vanity/lib/vanity.rb
+++ ruby/vanity/lib/vanity.rb
@@ -1,17 +1,17 @@
-require_relative 'vanity/constants.rb'
+require_relative "vanity/constants.rb"
 module Vanity
-  def self.find_number(word)
-    num = ''
-    word.split('').each do |c|
-      num += '2' if c == 'a' || c == 'b' || c == 'c'
-      num += '3' if c == 'd' || c == 'e' || c == 'f'
-      num += '4' if c == 'g' || c == 'h' || c == 'i'
-      num += '5' if c == 'j' || c == 'k' || c == 'l'
-      num += '6' if c == 'm' || c == 'n'  || c == 'o'
-      num += '7' if c == 'p' || c == 'q' || c == 'r' || c == 's'
-      num += '8' if c == 't' || c == 'u' || c == 'v'
-      num += '9' if c == 'w' || c == 'x' || c == 'y' || c == 'z'
+  def self.find_nber(word)
+    n = ""
+    word.split("").each do |c|
+      n = n + "2" if c == "a" || c == "b" || c == "c"
+      n = n + "3" if c == "d" || c == "e" || c == "f"
+      n = n + "4" if c == "g" || c == "h" || c == "i"
+      n = n + "5" if c == "j" || c == "k" || c == "l"
+      n = n + "6" if c == "m" || c == "n"  || c == "o"
+      n = n + "7" if c == "p" || c == "q" || c == "r" || c == "s"
+      n = n + "8" if c == "t" || c == "u" || c == "v"
+      n = n + "9" if c == "w" || c == "x" || c == "y" || c == "z"
     end
   end
 end
-puts Vanity::find_number("example")
+puts Vanity::find_nber("example")
